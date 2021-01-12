37d4b32de932c33f1d5501369f9fcbcbea0a4a68
Cleidiane Eloy <cleidiane.re@aluno.ifsc.edu.br>
2021-01-12 17:08:11 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
+    <>
         <NavBar />
-    </div>
+        <Home />
+    </>
   );
 }
 

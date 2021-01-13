c4d73206e3d1767bfaa18854c5737da378557c9a
Cleidiane Eloy <cleidiane.re@aluno.ifsc.edu.br>
2021-01-12 22:28:32 -0300

    Add routes and Steps

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import StepsHeader from './StepsHeader';
+import './style.css';
+function Orders(){
+    return (
+        <div>
+            <StepsHeader />
+        </div>
+    )
+
+    
+}
+
+export default Orders;
